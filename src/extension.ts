bf74855eee7aee5607ddd66ad50cf233b213cc7b
CuddlySheep <40072961+CuddlySheep@users.noreply.github.com>
2021-10-24 11:22:31 +0200

    feat(vscode-ex): Added multi-root workspace support
    closes #9

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { GitexFlowTreeProvider } from './gitexTreeView';
 import { GFlowExtension } from './gflowext/GFlowExtension';
+import { GFlow } from 'gitex-flow';
 
 /**
  * This method is called when your extension is activated.
@@ -11,55 +12,57 @@ import { GFlowExtension } from './gflowext/GFlowExtension';
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
   vscode.window.registerTreeDataProvider('gitexFlowActionsTreeView', new GitexFlowTreeProvider());
 
-  const workspaceFolder = await getWorkspaceFolder();
-  let extension = await GFlowExtension.create(workspaceFolder.uri.fsPath);
-  const configWatcher = vscode.workspace.createFileSystemWatcher('**/*.{gitex,gitex.json,gitex-flow,gitex-flow.json}');
-  configWatcher.onDidChange(async function () {
-    extension = await GFlowExtension.create(workspaceFolder.uri.fsPath);
-    vscode.window.showInformationMessage(`The configuration of gitex-flow was refreshed.`);
-  });
-
   const initDisposable = vscode.commands.registerCommand('gitex-flow.init', async function () {
+    const extension = await createGFlow();
     await extension.init();
   });
 
   const featureStartDisposable = vscode.commands.registerCommand('gitex-flow.feature.start', async function () {
+    const extension = await createGFlow();
     await extension.feature.start('#<issue>');
   });
   const featureFinishDisposable = vscode.commands.registerCommand('gitex-flow.feature.finish', async function () {
+    const extension = await createGFlow();
     await extension.feature.finish();
   });
 
   const bugfixStartDisposable = vscode.commands.registerCommand('gitex-flow.bugfix.start', async function () {
+    const extension = await createGFlow();
     await extension.bugfix.start('#<issue>');
   });
   const bugfixFinishDisposable = vscode.commands.registerCommand('gitex-flow.bugfix.finish', async function () {
+    const extension = await createGFlow();
     await extension.bugfix.finish();
   });
 
   const releaseStartDisposable = vscode.commands.registerCommand('gitex-flow.release.start', async function () {
+    const extension = await createGFlow();
     await extension.release.start();
   });
   const releaseFinishDisposable = vscode.commands.registerCommand('gitex-flow.release.finish', async function () {
+    const extension = await createGFlow();
     await extension.release.finish();
   });
 
   const hotfixStartDisposable = vscode.commands.registerCommand('gitex-flow.hotfix.start', async function () {
+    const extension = await createGFlow();
     await extension.hotfix.start();
   });
   const hotfixFinishDisposable = vscode.commands.registerCommand('gitex-flow.hotfix.finish', async function () {
+    const extension = await createGFlow();
     await extension.hotfix.finish();
   });
 
   const supportStartDisposable = vscode.commands.registerCommand('gitex-flow.support.start', async function () {
+    const extension = await createGFlow();
     await extension.support.start('2.0.0-lts');
   });
   const supportStopDisposable = vscode.commands.registerCommand('gitex-flow.support.finish', async function () {
+    const extension = await createGFlow();
     await extension.support.finish();
   });
 
   context.subscriptions.push(
-    configWatcher,
     initDisposable,
     featureStartDisposable,
     featureFinishDisposable,
@@ -80,6 +83,16 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
  * This method is called when your extension is deactivated.
  */
 export async function deactivate(): Promise<void> {}
+
+/**
+ * Creates GFlow instance.
+ * 
+ * @returns A promise on the GFlow instance. 
+ */
+ async function createGFlow(): Promise<GFlow> {
+  const workspaceFolder = await getWorkspaceFolder();
+  return GFlowExtension.create(workspaceFolder.uri.fsPath);
+}
 
 /**
  * Gets the workspace path.
